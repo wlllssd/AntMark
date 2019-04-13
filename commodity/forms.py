2f80b92405848f695479f460fde51b90375aa8c0
black-rice <44921502+blackRice-Tu@users.noreply.github.com>
2019-04-13 21:59:46 +0800

    增加商品功能

---
+++ commodity/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from .models import CommodityTag, Commodity
+
+class CommodityTagForm(forms.ModelForm):
+    class Meta:
+        model = CommodityTag
+        fields = ('tag', )
+
+
+class CommodityForm(forms.ModelForm):
+    class Meta:
+        model = Commodity
+        fields = ('title', 'author', 'body', 'price', 'image')
--- commodity/models.py
+++ commodity/models.py
@@ -1,3 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
-# Create your models here.
+class CommodityTag(models.Model):
+	tag = models.CharField(max_length = 300)
+
+	def __str__(self):
+		return self.tag
+
+class Commodity(models.Model):
+	title = models.CharField(max_length = 300)
+	author = models.ForeignKey(User, related_name = "commodity", on_delete = models.CASCADE)
+	commodity_tag = models.ManyToManyField(CommodityTag, related_name = "commodity_tag", blank = True)
+	body = models.TextField()
+	price = models.DecimalField(max_digits=7, decimal_places=2)
+	image = models.ImageField(upload_to = 'images/%Y/%m/%d')
+	created = models.DateTimeField(default = timezone.now)
+	updated = models.DateTimeField(auto_now = True)
+
+	class Meta:
+		ordering = ('-updated',)
+
+	def __str__(self):
+		return self.title
