7d46e89b1154803ef0d1410a0837ef8c56884a8e
black-rice <804163912@qq.com>
2019-04-21 23:10:16 +0800

    对商品显示界面做简单修改

--- commodity/views.py
+++ commodity/views.py
@@ -53,21 +53,6 @@ def commodity_detail(request, id):
     context = {"commodity":commodity, "similar_commodities":similar_commodities}
     return render(request, "commodity/common/commodity_detail.html", context)
 
-# def article_detail(request, id):
-#     article = get_object_or_404(ArticlePost, id=id)
-    
-#     if request.method == "POST":
-#         comment_form = CommentForm(data=request.POST) 
-#         if comment_form.is_valid():
-#             new_comment = comment_form.save(commit=False) 
-#             new_comment.article = article 
-#             new_comment.save()
-#     else:
-#         comment_form = CommentForm()
-#     article_tags_ids = article.article_tag.values_list("id", flat=True)
-#     similar_articles = ArticlePost.objects.filter(article_tag__in=article_tags_ids).exclude(id=article.id)
-#     similar_articles = similar_articles.annotate(same_tags=Count("article_tag")).order_by('-same_tags', '-created')[:4]
-#     return render(request, "article/list/article_content.html", {"article":article, "comment_form":comment_form, "similar_articles":similar_articles})
 
 # 个人商品库
 @login_required(login_url = '/users/login')
@@ -130,6 +115,7 @@ def create_commodity(request):
         sources = CommoditySource.objects.all()
         context = {'commodity_form':commodity_form, 'tags':tags, 'sources':sources}
         return render(request, 'commodity/personal/create_commodity.html', context)
+
 
 # 编辑商品信息
 @login_required(login_url = '/users/login')
