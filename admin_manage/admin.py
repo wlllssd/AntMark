49bc44cb202b2daffccb6d318e1884aef81206be
black-rice <804163912@qq.com>
2019-05-08 00:43:48 +0800

    增加公告功能

--- admin_manage/admin.py
+++ admin_manage/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Announcement
 
-# Register your models here.
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('title', 'timestamp')
+admin.site.register(Announcement, AnnouncementAdmin)
--- admin_manage/models.py
+++ admin_manage/models.py
@@ -2,8 +2,11 @@ from django.db import models
 
 # Create your models here.
 class Announcement(models.Model):
-    text = models.CharField(max_length=2000)
+    title = models.CharField(max_length=200, blank = False, default = None)
+    text = models.CharField(max_length=20000)
     timestamp = models.DateTimeField(auto_now_add=True)
     
+    class Meta:
+        ordering = ('-timestamp', )
     def __str__(self):
         return self.text
