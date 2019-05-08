9fb4a1c6db87de9a6e73ca2f67625dc1ae55f79b
black-rice <804163912@qq.com>
2019-05-08 13:06:40 +0800

    修改商品审核提示框

--- commodity/urls.py
+++ commodity/urls.py
@@ -3,10 +3,10 @@ from . import views
 
 app_name = "commodity"
 urlpatterns = [
-    path('tag-list/', views.tag_list, name = "tag_list"),
-    path('source-list/', views.source_list, name = "source_list"),
     path('commodity-list/', views.commodity_list, name = "commodity_list"),
     path('commodity-detail/<int:commodity_id>/', views.commodity_detail, name = "commodity_detail"),
+    path('search-commodity/', views.search_commodity, name = "search_commodity"),
+    
     path('commodity-repertory/', views.commodity_repertory, name = "commodity_repertory"),
     path('edit-commodity/<int:commodity_id>/', views.edit_commodity, name = "edit_commodity"),
     path('preview-commodity/<int:commodity_id>/', views.preview_commodity, name = "preview_commodity"),
@@ -17,7 +17,6 @@ urlpatterns = [
     path('put-off-commodity/', views.put_off_commodity, name = "put_off_commodity"),
     path('put-on-commodity/', views.put_on_commodity, name = "put_on_commodity"),
     path('not-verified-list/', views.not_verified_list, name = "not_verified_list"),
-    path('commodity-verify/<int:commodity_id>/', views.commodity_verify, name="commodity_verify"),
-    path('search-commodity/', views.search_commodity, name = "search_commodity"),
+    path('commodity-verify/', views.commodity_verify, name="commodity_verify"),
     path('commodity-image/<int:commodity_id>/', views.commodity_image, name = "commodity_image"),
 ]
